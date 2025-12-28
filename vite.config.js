7363fa41a09b49fca67ad4a27dd747728e0d3b22
kennyklfok <kennyklfok@gmail.com>
2025-12-28 15:04:57 -0800

    fixed folder structure and hosting

--- vite.config.js
+++ vite.config.js
@@ -2,17 +2,21 @@ import { defineConfig } from "vite";
 import { resolve } from "path";
 
 export default defineConfig({
+  root: ".",
+  publicDir: "public",
   build: {
+    outDir: "dist",
+    emptyOutDir: true,
     rollupOptions: {
       input: {
-        main: resolve(__dirname, "index.html"),               // root index
-        ecg: resolve(__dirname, "public/ecg.html"),
-        holter: resolve(__dirname, "public/holter.html"),
-        ett: resolve(__dirname, "public/ett.html"),
-        pmc: resolve(__dirname, "public/pmc.html"),
-        cpc: resolve(__dirname, "public/cpc.html"),
-        hh: resolve(__dirname, "public/hh.html"),
-        drs: resolve(__dirname, "public/drs.html")
+        main: resolve(__dirname, "index.html"),
+        ecg: resolve(__dirname, "ecg.html"),
+        holter: resolve(__dirname, "holter.html"),
+        ett: resolve(__dirname, "ett.html"),
+        pmc: resolve(__dirname, "pmc.html"),
+        cpc: resolve(__dirname, "cpc.html"),
+        hh: resolve(__dirname, "hh.html"),
+        drs: resolve(__dirname, "drs.html")
       }
     }
   }
